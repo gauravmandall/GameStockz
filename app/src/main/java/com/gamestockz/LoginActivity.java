a7d9e0e2e43307d7c9f1868ebcb1179aa89f1a48
Indian-Hacker <72154932+Indian-hacker@users.noreply.github.com>
2021-06-21 14:24:42 +0530

    Add some new Features & UI

--- app/src/main/java/com/gamestockz/LoginActivity.java
+++ app/src/main/java/com/gamestockz/LoginActivity.java
@@ -1,62 +1,31 @@
 package com.gamestockz;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentManager;
-import androidx.viewpager.widget.ViewPager;
-import androidx.viewpager2.adapter.FragmentStateAdapter;
-import androidx.viewpager2.widget.ViewPager2;
-
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 
-import com.google.android.material.tabs.TabLayout;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity {
-
-    TabLayout tabLayout;
-    ViewPager2 pager2;
-    ViewPagerAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+//        for changing status bar icon color
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+        }
         setContentView(R.layout.activity_login);
-
-        tabLayout = findViewById(R.id.tabLayout);
-        pager2 = findViewById(R.id.viewPager2);
-
-
-        FragmentManager fm = getSupportFragmentManager();
-        adapter = new ViewPagerAdapter(fm, getLifecycle());
-        pager2.setAdapter(adapter);
-
-
-        tabLayout.addTab(tabLayout.newTab().setText("Log In"));
-        tabLayout.addTab(tabLayout.newTab().setText("Sign Up"));
-
-        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
-            @Override
-            public void onTabSelected(TabLayout.Tab tab) {
-                pager2.setCurrentItem(tab.getPosition());
-            }
-
-            @Override
-            public void onTabUnselected(TabLayout.Tab tab) {
-
-            }
-
-            @Override
-            public void onTabReselected(TabLayout.Tab tab) {
-
-            }
-        });
-
-
-        pager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
-            @Override
-            public void onPageSelected(int position) {
-                tabLayout.selectTab(tabLayout.getTabAt(position));
-            }
-        });
-
     }
+    public void newUserRegisterNow(View view) {
+        startActivity(new Intent(this, SignupActivity.class));
+        overridePendingTransition(R.anim.slide_in_right, R.anim.stay);
+    }
+
+
+
+
+
+
 }
--- app/src/main/java/com/gamestockz/SignupActivity.java
+++ app/src/main/java/com/gamestockz/SignupActivity.java
@@ -8,6 +8,7 @@ import android.view.Window;
 import android.view.WindowManager;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 public class SignupActivity extends AppCompatActivity {
 
@@ -19,22 +20,23 @@ public class SignupActivity extends AppCompatActivity {
         changeStatusBarColor();
 
     }
-
     public void changeStatusBarColor() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
 
             Window window = getWindow();
             window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-            window.setStatusBarColor(getResources().getColor(R.color.blue_900));
+            window.setStatusBarColor(ContextCompat.getColor(getApplicationContext(),R.color.blue_900));
+
 
             getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
         }
     }
-
-    public void onSignupClick(View view) {
+    public void alreadyHaveAccount(View view) {
         startActivity(new Intent(this, LoginActivity.class));
         overridePendingTransition(R.anim.slide_in_left, android.R.anim.slide_out_right);
     }
+
+
 
 
 }
