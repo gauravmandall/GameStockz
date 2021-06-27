91af269669da0b676c051fddb864c08fabdfa1a9
Indian-Hacker <72154932+Indian-hacker@users.noreply.github.com>
2021-06-27 12:53:33 +0530

    Bugs Found But Didn't Resolved

---
+++ app/src/main/java/com/gamestockz/Activities/WithdrawActivity.java
@@ -0,0 +1,16 @@
+package com.gamestockz.Activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.gamestockz.R;
+
+public class WithdrawActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_withdraw);
+    }
+}
