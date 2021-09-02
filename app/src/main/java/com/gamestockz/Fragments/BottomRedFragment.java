256c32dadc92405433c9ef3f6d87b1ccf2085352
Indian-Hacker <72154932+Indian-hacker@users.noreply.github.com>
2021-09-02 15:57:22 +0530

    initial commit

--- app/src/main/java/com/gamestockz/Fragments/BottomRedFragment.java
+++ app/src/main/java/com/gamestockz/Fragments/BottomRedFragment.java
@@ -1,54 +1,134 @@
 package com.gamestockz.Fragments;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.gamestockz.Activities.WithdrawActivity;
 import com.gamestockz.R;
 import com.gamestockz.databinding.FragmentBottomRedBinding;
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+import com.google.android.material.button.MaterialButtonToggleGroup;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.SplittableRandom;
 
 public class BottomRedFragment extends BottomSheetDialogFragment {
 
     FragmentBottomRedBinding binding;
+    ProgressDialog progressDialog;
 
-    //    Button tenred,fiftyred,hundredred;
     FirebaseDatabase database;
     DatabaseReference myref;
     DatabaseReference price;
 
     int quantity = 0;
+    int quantityRed;
+    String coin;
+    int icoin;
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         binding = FragmentBottomRedBinding.inflate(inflater, container, false);
         View view = binding.getRoot();
+        String qty = String.valueOf(quantity);
+        binding.quantityRed.setText(qty);
+        quantityRed = Integer.parseInt(binding.quantityRed.getText().toString());
 
+        progressDialog = new ProgressDialog(getActivity());
+        progressDialog.setTitle("Joining Red");
+        progressDialog.setMessage("Please Wait");
+
+        binding.toggleButton.addOnButtonCheckedListener(new MaterialButtonToggleGroup.OnButtonCheckedListener() {
+            @Override
+            public void onButtonChecked(MaterialButtonToggleGroup group, int checkedId, boolean isChecked) {
+
+                if (group.getCheckedButtonId() == R.id.btn1) {
+
+                    icoin = 10;
+                    int ten = icoin * quantity;
+                    coin = String.valueOf(ten);
+                    binding.totalPriceMoney.setText(coin);
+                } else if (group.getCheckedButtonId() == R.id.btn2) {
+
+                    icoin = 50;
+                    int fifty = icoin * quantity;
+                    coin = String.valueOf(fifty);
+                    binding.totalPriceMoney.setText(coin);
+                } else if (group.getCheckedButtonId() == R.id.btn3) {
+
+                    icoin = 100;
+                    int hundred = icoin * quantity;
+                    coin = String.valueOf(hundred);
+                    binding.totalPriceMoney.setText(coin);
+                }
+            }
+        });
         binding.subtractRed.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
                 decrement();
-
+            }
+        });
+        binding.addRed.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                increment();
             }
         });
 
+        binding.joinRed.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (quantityRed == 0 || icoin == 0){
+                    Toast.makeText(getActivity(), "Please choose Correct Values", Toast.LENGTH_SHORT).show();
+
+                }else{
+                    progressDialog.show();
+                    progressDialog.setCanceledOnTouchOutside(false);
+                    joinVerify();
+                }
+            }
+        });
 
         return view;
     }
 
-    private void decrement() {
+    private void joinVerify() {
 
-        quantity = quantity - 1;
-        binding.subtractRed.setText(" " + quantity);
+//        waiting for Mobile Number
+
+
+    }
+
+    private void increment() {
+        quantity++;
+        String s = String.valueOf(quantity);
+        binding.quantityRed.setText(s);
+
+        int calc = icoin * quantity;
+
+        coin = String.valueOf(calc);
+        binding.totalPriceMoney.setText(coin);
+
+    }
+
+    private void decrement() {
+        quantity = quantity > 0 ? --quantity : 0;
+        String s = String.valueOf(quantity);
+        binding.quantityRed.setText(s);
+
+        int calc = icoin * quantity;
+        coin = String.valueOf(calc);
+        binding.totalPriceMoney.setText(coin);
+
     }
 
 }
